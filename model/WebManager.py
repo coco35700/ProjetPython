ba8609b497e71d0c6c83386d53563cd3c279fb67
coco35700 <jezequel35@gmail.com>
2015-03-12 15:31:02 +0100

    maj

--- model/WebManager.py
+++ model/WebManager.py
@@ -19,37 +19,49 @@ class WebManager():
 	def activity(self):
 		conn = sqlite3.connect(dataBasePath)
 		c = conn.cursor();
-		chaine = ""
+		
+		bandeau = "<tr><th>inseeNb</th><th>comLib</th><th>equipmentId</th><th>equNbEquIdentique</th>"
+		bandeau = bandeau + "<th>actCode</th><th>actLib</th><th>quActivitePraticable</th>"
+		bandeau = bandeau + "<th>equActivitePratique</th><th>equActiviteSalleSpe</th><th>actNivLib</th></tr>"
+		chaine = "<table style='width:100%' border='1'>"+bandeau
 
-		for item in c.execute("select * from activity"):
-			chaine = chaine+" activity : "+str(item)+"</br>"
+		for line in c.execute("select * from activity").fetchall():
+			chaine = chaine + "<tr>"
+			for elem in range(len(line)):
+				chaine = chaine+"<td>"+line[elem]+"</td>"
 
-		return chaine
+			chaine = chaine + "</tr>"
+		return chaine+"</table>"
 
 	@cherrypy.expose
 	def equipment(self):
 		conn = sqlite3.connect(dataBasePath)
 		c = conn.cursor();
-		chaine = ""
 
-		for item in c.execute("select * from equipment"):
-			chaine = chaine+" equipments : "+str(item)+"</br>"
+		bandeau = "<tr><th>comInsee</th><th>comLib</th><th>equipmentFile</th><th>equAnneeService</th>"
+		bandeau = bandeau + "<th>equNom</th><th>equNomBatiment </th></tr>"
+		chaine = "<table style='width:100%' border='1'>"+bandeau
 
-		return chaine
+		for line in c.execute("select * from equipment").fetchall():
+			chaine = chaine + "<tr>"
+			for elem in range(len(line)):
+				chaine = chaine+"<td>"+line[elem]+"</td>"
+
+			chaine = chaine + "</tr>"
+		return chaine+"</table>"
 
 	@cherrypy.expose
 	def installation(self):
 		conn = sqlite3.connect(dataBasePath)
 		c = conn.cursor();
-		bandeau = "<tr><th>comLib</th><th>comInsee</th><th>insCodePostal</th><th>insLieuDit</th>"+"<th>insNoVoie</th><th>insLibelleVoie</th><th>nb_Equipements</th><th>nb_FicheEquipement</th></tr>"
+		bandeau = "<tr><th>comLib</th><th>comInsee</th><th>insCodePostal</th><th>insLieuDit</th>"
+		bandeau = bandeau + "<th>insNoVoie</th><th>insLibelleVoie</th><th>nb_Equipements</th><th>nb_FicheEquipement</th></tr>"
 		chaine = "<table style='width:100%' border='1'>"+bandeau
 
-
-		for item in c.execute("select * from installations"):
-			tab = str(item).replace("'","").replace("(","").replace(")","").split(",")
+		for line in c.execute("select * from installations").fetchall():
 			chaine = chaine + "<tr>"
-			for i in range(len(tab)):
-				chaine = chaine+"<td>"+tab[i]+"</td>"
+			for elem in range(len(line)):
+				chaine = chaine+"<td>"+line[elem]+"</td>"
 
 			chaine = chaine + "</tr>"
 		return chaine+"</table>"
